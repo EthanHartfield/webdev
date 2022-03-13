5a7cc39deebbbe41d9b5eab86bc099a681c86702
EthanHartfield <97472701+EthanHartfield@users.noreply.github.com>
2022-03-13 18:54:02 -0500

    Add files via upload

--- ClientSideProgramming/Module3/Module3Exercise/cards.js
+++ ClientSideProgramming/Module3/Module3Exercise/cards.js
@@ -3,18 +3,19 @@ function printCard() {
    var name_line = "<strong>Name: </strong>" + this.name + "<br>\n";
    var email_line = "<strong>Email: </strong>" + this.email + "<br>\n";
    var address_line = "<strong>Address: </strong>" + this.address + "<br>\n";
-   var phone_line = "<strong>Phone: </strong>" + this.phone + "<hr>\n";
-   document.write(name_line, email_line, address_line, phone_line);
+   var phone_line = "<strong>Phone: </strong>" + this.phone + "<br>\n";
+   var personalNotes_line = "<strong>Personal Notes: </strong>" + this.personalNotes + "<hr>\n";
+   document.write(name_line, email_line, address_line, phone_line, personalNotes_line);
 }
 
 //Defining a card object using a constructor function
 //by passing 4 parameters
-function Card(name,email,address,phone,personal_notes) {
+function Card(name,email,address,phone,personalNotes) {
    this.name = name;
    this.email = email;
    this.address = address;
    this.phone = phone;
-   this.personal_notes = personal_notes;
+   this.personalNotes = personalNotes;
    this.printCard = printCard;
 }
 
@@ -22,15 +23,18 @@ function Card(name,email,address,phone,personal_notes) {
 var sue = new Card("Sue Suthers", 
                    "sue@suthers.com", 
                    "123 Elm Street, Yourtown ST 99999", 
-                   "555-555-9876");
+                   "555-555-9876",
+                   "She's cool I guess.");
 var fred = new Card("Fred Fanboy", 
                     "fred@fanboy.com", 
                     "233 Oak Lane, Sometown ST 99399", 
-                    "555-555-4444");
+                    "555-555-4444",
+                    "He's also cool I guess.");
 var jimbo = new Card("Jimbo Jones", 
                      "jimbo@jones.com", 
                      "233 Walnut Circle, Anotherville ST 88999", 
-                     "555-555-1344");
+                     "555-555-1344",
+                     "He's also also cool I guess.");
 
 // Now print them
 sue.printCard();
